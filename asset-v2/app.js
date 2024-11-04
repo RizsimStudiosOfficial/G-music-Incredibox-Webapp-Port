2e052d713fe53e2110dec6120c8543a6143fe2b4
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2024-11-04 01:13:29 -0800

    this could fix the ugly loop shit

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,6 +1,6 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
-    this.name = "G Music (OG)", this.version = "2", this.date = "2013", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 139, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#5c5c5c", this.col0 = "#919191", this.col1 = "#cccccc", this.col2 = "#4f4f4f", this.col3 = "#d9d9d9", this.col4 = "#666666", this.animearray = [{
+    this.name = "G Music (OG)", this.version = "2", this.date = "2013", this.folder = "asset-v2/", this.looptime = , this.bpm = 139, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#5c5c5c", this.col0 = "#919191", this.col1 = "#cccccc", this.col2 = "#4f4f4f", this.col3 = "#d9d9d9", this.col4 = "#666666", this.animearray = [{
         name: "b0-og",
         color: "828282",
         uniqsnd: !0
